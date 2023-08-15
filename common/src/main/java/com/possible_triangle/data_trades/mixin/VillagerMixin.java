4819d44fecab97421cba2f728a259a72ee0e71bd
Niklas Widmann <nik@somethingcatchy.net>
2023-08-15 11:33:37 +0200

    remove @Shadow usage

--- common/src/main/java/com/possible_triangle/data_trades/mixin/VillagerMixin.java
+++ common/src/main/java/com/possible_triangle/data_trades/mixin/VillagerMixin.java
@@ -11,13 +11,10 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(Villager.class)
 public abstract class VillagerMixin {
 
-    @Shadow
-    public abstract VillagerData getVillagerData();
-
     @ModifyConstant(method = "updateTrades()V", constant = @Constant(intValue = 2))
     private int injected(int value) {
         var self = (Villager) (Object) this;
-        return ProfessionReloader.takeTradesAmount(self, getVillagerData().getLevel()).orElse(value);
+        return ProfessionReloader.takeTradesAmount(self, self.getVillagerData().getLevel()).orElse(value);
     }
 
 }
